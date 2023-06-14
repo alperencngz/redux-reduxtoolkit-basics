18d17165f0c328b7856aabeb730296f1323abf86
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-14 12:47:42 +0300

    Managing component-wide visibility state
    And added an increment which takes an amount insite action

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,9 +5,14 @@ import classes from './Counter.module.css';
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 5 });
   };
 
   const decrementHandler = () => {
@@ -15,14 +20,17 @@ const Counter = () => {
   };
 
 
-  const toggleCounterHandler = () => { };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" })
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,34 @@
 import { createStore } from "redux";
 
-const counterRecuder = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true }
+
+const counterRecuder = (state = initialState, action) => {
 
     if (action.type === "increment") {
         return {
-            counter: state.counter + 1
+            counter: state.counter + 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === "increase") {
+        return {
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
         }
     }
 
     if (action.type === "decrement") {
         return {
-            counter: state.counter - 1
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === "toggle") {
+        return {
+            showCounter: !state.showCounter,
+            counter: state.counter
         }
     }
 
