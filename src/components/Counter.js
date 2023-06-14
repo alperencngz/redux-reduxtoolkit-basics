cd408e50ee18649e7b223a05bfefe463d66efbbf
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-06-14 13:21:03 +0300

    Added a ref to be able to use Increase more flexible
    We did the part for redux where we take an amount and increase the value in store by it, but we didn't make the value the action receiving from component flexible so I did it here with ref

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,18 +1,21 @@
 import { useSelector, useDispatch } from "react-redux";
 
 import classes from './Counter.module.css';
+import { useRef } from "react";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
   const showCounter = useSelector(state => state.showCounter);
 
+  const increaseRef = useRef(0);
+
   const incrementHandler = () => {
     dispatch({ type: "increment" });
   };
 
-  const increaseHandler = () => {
-    dispatch({ type: "increase", amount: 5 });
+  const increaseHandler = (event) => {
+    dispatch({ type: "increase", amount: Number(increaseRef.current.value) });
   };
 
   const decrementHandler = () => {
@@ -30,7 +33,9 @@ const Counter = () => {
       {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
-        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={increaseHandler}>Increase Value</button>
+        <label> Increase Value </label>
+        <input type="text" ref={increaseRef}></input>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
